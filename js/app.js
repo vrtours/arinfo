c10767b7ebc095cddcb375de995c77899520b24c
Gerard Slee <ddutchie@gmail.com>
2021-03-01 12:41:35 +0200

    Some changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Builds.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/arinfo.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
